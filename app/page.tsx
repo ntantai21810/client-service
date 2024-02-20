eda6b73ace0dee4e901edafe78c954458757ff06
Tai TN <tai.tn@clv.local>
2024-02-20 22:33:59 +0700

    init proj

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 export default function Home() {
+  const [socketClient, setSocketClient] = useState<any>(null);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3001");
+
+    setSocketClient(socket);
+
+    console.log(socket);
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button
+        onClick={() => {
+          socketClient.emit("message", "Hello from client");
+        }}
+      >
+        Test socket
+      </button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
